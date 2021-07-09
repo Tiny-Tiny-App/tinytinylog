170e823c3b40b0768e76f973535251f6fd628225
Yelluw <pr@yelluw.com>
2021-07-09 16:56:18 -0400

    landing page

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import IndexView, UserProfileView
+from .views import IndexView, UserProfileView, UserDeleteView, UserAccountDeletedView
 
 
 urlpatterns = [
     path('accounts/user/profile/', UserProfileView.as_view(), name='user_profile'),
+    path('accounts/user/d/', UserDeleteView.as_view(), name='user_delete'),
+    path('goodbye/', UserAccountDeletedView.as_view(), name='goodbye'),
     path('', IndexView.as_view(), name='index'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,10 @@
 import logging
 from django.views.generic import View, TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse_lazy
+
 
 logger = logging.getLogger(__name__)
 
@@ -8,9 +13,18 @@ class IndexView(TemplateView):
     template_name = 'index.html'
 
 
-class UserProfileView(TemplateView):
+class UserProfileView(LoginRequiredMixin, TemplateView):
     template_name = 'user/profile.html'
 
 
-class UserDeleteView(View):
-    pass
+class UserDeleteView(LoginRequiredMixin, View):
+    def delete_user(self):
+        self.request.user.delete()
+
+    def post(self, *args, **kwargs):
+        self.delete_user()
+        return redirect(reverse_lazy('goodbye'))
+
+
+class UserAccountDeletedView(TemplateView):
+    template_name = 'user/deleted.html'
